0b2c9d9f14ac9d409acaf80771b02acf81f53798
Luan Silveira <luanmax270@gmail.com>
2022-09-05 19:52:42 -0300

    conclusão dos cards com javascript.

--- js/script.js
+++ js/script.js
@@ -24,6 +24,18 @@ buscar.addEventListener = () => {
   efetuar.classList.remove("active");
 };
 
+// Pagina do Card
+const openCard = document.querySelector("#card-compra");
+const openProdutos = document.querySelector(".card-produtos");
+
+openCard.addEventListener("click", function () {
+  openProdutos.classList.toggle("active");
+});
+
+openCard.addEventListener = () => {
+  openProdutos.classList.remove("active");
+};
+
 // Pagina de Login
 
 const admin = document.querySelector("#campo-login");
@@ -78,3 +90,108 @@ var swiper = new Swiper(".container-clientes", {
     },
   },
 });
+
+//Animação //
+if (window.SimpleAnime) {
+  new SimpleAnime()
+}
+
+
+
+// etapa de produtos feito com javascript importando informações de um arquivo json ! //
+// exibir produtos com json//
+
+let cart = [];
+let modalQt = 1
+let modalkey = 0
+
+
+
+// Produtos//
+produtosJson.map((item, index) => {
+  let containerProduto = document.querySelector(".modelo-produto .produtos").cloneNode(true);
+  containerProduto.setAttribute("data-key", index);
+  containerProduto.querySelector(".imagem-produtos img").src = item.img;
+  containerProduto.querySelector(".titulo-modelo").innerHTML = item.name;
+  containerProduto.querySelector(".descricao-modelo").innerHTML = `R$ ${item.price.toFixed(2)}`;
+  containerProduto.querySelector(".bx").addEventListener("click", (e) => {
+    e.preventDefault();
+    let key = e.target.closest(".produtos").getAttribute("data-key");
+    modalkey = key
+    let identifier = produtosJson[modalkey].id+'@';
+    let product = cart.findIndex((item)=>item.identifier == identifier);
+    if(product > -1) {
+     //cart[key].qt += modalQt
+    } else {
+      cart.push({
+        identifier,
+        id:produtosJson[modalkey].id,
+        qt:modalQt
+      })
+    }
+    atualizarCard()
+    
+  });
+  document.querySelector(".container-produtos").append(containerProduto);
+});
+
+
+
+// Atualização do carrinho//
+
+function atualizarCard() {
+  document.querySelector('.itens-carrinhos-compra span').innerHTML = cart.length
+
+  if(cart.length < 1) {
+    openProdutos.classList.remove("active");
+  }
+ 
+  document.querySelector('.carrinho-de-compra').innerHTML = '';
+
+  let total = 0;
+
+  for(let i in cart) {
+    let produtosAdicionado = produtosJson.find((item)=>item.id == cart[i].id)
+    total += produtosAdicionado.price * cart[i].qt;
+    console.log(produtosAdicionado)
+    let ItemDoCart = document.querySelector('.modelo-produto .container-cards-produtos').cloneNode(true);
+     
+    ItemDoCart.querySelector('img').src = produtosAdicionado.img;
+    ItemDoCart.querySelector('.card-produto-nome').innerHTML = produtosAdicionado.name;
+    ItemDoCart.querySelector('.card-item-quantidade').innerHTML = cart[i].qt;
+    ItemDoCart.querySelector('.quantidade-menos').addEventListener('click', ()=>{
+      if(cart[i].qt > 1) {
+        cart[i].qt--;
+      } else {
+        cart.splice(i, 1)
+      }
+      atualizarCard()
+    })
+
+    ItemDoCart.querySelector('.quantidade-mais').addEventListener('click', ()=>{
+      cart[i].qt++
+      atualizarCard()
+    })
+
+     document.querySelector('.carrinho-de-compra').append(ItemDoCart);
+  }
+
+  document.querySelector('.total span:last-child').innerHTML = `R$: ${total.toFixed(2)}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
